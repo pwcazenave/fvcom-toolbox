479804e6a8a9e1ae51189976393eb7b887b00750
Pierre Cazenave <pwcazenave@gmail.com>
2012-10-16 14:22:00 +0100

    Transpose the lat and longitude grids to match their geographic orientation

--- fvcom_prepro/read_NCEP_wind.m
+++ fvcom_prepro/read_NCEP_wind.m
@@ -88,7 +88,7 @@ lon_varid = netcdf.inqVarID(nc_u10, 'lon');
 nceplatvector = netcdf.getVar(nc_u10, lat_varid);
 nceplonvector = netcdf.getVar(nc_u10, lon_varid);
 
-[nceplon, nceplat] = meshgrid(nceplonvector, nceplatvector);
+[nceplon, nceplat] = meshgrid(nceplonvector', nceplatvector');
 
 % Find the necessary variables
 u10_varid_NCEP = netcdf.inqVarID(nc_u10, 'uwnd');
