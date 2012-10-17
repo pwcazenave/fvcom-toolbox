aa5d904131a05b06ee90bd1fd0aa47b1512f5928
Pierre Cazenave <pwcazenave@gmail.com>
2012-10-17 11:03:14 +0100

    Add the new output description (the time vector) and correct the revision history

--- fvcom_prepro/read_NCEP_wind.m
+++ fvcom_prepro/read_NCEP_wind.m
@@ -17,12 +17,13 @@ function [nceplon, nceplat, ncep_u10, ncep_v10, nceptime] = read_NCEP_wind(ncep_
 %   nceplat - latitude data
 %   ncep_u10 - u vector component 10 m above surface
 %   ncep_v10 - v vector component 10 m above surface
+%   nceptime - time in Modified Julian Days
 % 
 % Author(s)
 %   Pierre Cazenave (Plymouth Marine Laboratory)
 % 
 % Revision history:
-%   2012-10-16 First version based on parts of ncep2fvcom_U10V10.m in the
+%   2012-10-16 First version based on parts of wrf2fvcom_U10V10.m in the
 %   fvcom-toolbox.
 % 
 %==========================================================================
