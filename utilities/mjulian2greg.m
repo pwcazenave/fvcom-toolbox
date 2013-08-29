5bb96b3d833ae7107e72d48df0ac8f61333a0e24
Pierre Cazenave <pwcazenave@gmail.com>
2013-08-29 15:46:59 +0100

    Add a marginally more helpful help at the beginning of the function.

--- utilities/mjulian2greg.m
+++ utilities/mjulian2greg.m
@@ -1,4 +1,6 @@
-function [year,month,day,hour,minu,sec,dayweek,dategreg] = julian2greg(MJD)
+function [year,month,day,hour,minu,sec,dayweek,dategreg] = mjulian2greg(MJD)
 % This function converts Modified Julian dates to Gregorian dates.
+%
+% [yr, mon, day, hr, mins, sec, dayweek, dategreg] = mjulian2greg(MJD);
 
 [year,month,day,hour,minu,sec,dayweek,dategreg] = julian2greg(MJD+2400000.5);
