83ebaa33e16e0d629cc3bfc9375d3d6c44f2a807
Pierre Cazenave <pwcazenave@gmail.com>
2013-09-04 18:28:18 +0100

    Start making the interpolation of the HYCOM data onto the vertical grid of FVCOM at the open boundaries work. This is quite a change from the POLCOMS version due in large part to the uniform vertical grid in HYCOM. The version also iterates through a variable list of fields to interpolate rather than having a hard-coded set (as is the case with the POLCOMS version).

--- fvcom_prepro/get_HYCOM_tsobc.m
+++ fvcom_prepro/get_HYCOM_tsobc.m
@@ -1,4 +1,4 @@
-function Mobj = get_HYCOM_tsobc(Mobj, hycom)
+function Mobj = get_HYCOM_tsobc(Mobj, hycom, varlist)
 % Read temperature, salinity, u and v data from the HYCOM model output
 % structure and interpolate onto the open boundaries in Mobj.
 %
@@ -14,10 +14,10 @@ function Mobj = get_HYCOM_tsobc(Mobj, hycom)
 %               - Mobj.lon, Mobj.lat - node coordinates (lat/long).
 %               - Mobj.obc_nodes - list of open boundary node inidices.
 %               - Mobj.nObcNodes - number of nodes in each open boundary.
-%   ts      = Cell array of PML POLCOMS-ERSEM NetCDF file(s) in which 4D
-%             variables of temperature and salinity (called 'temperature'
-%             and 'salinity') exist. Their shape should be (y, x, sigma,
-%             time).
+%   hycom   = Struct with HYCOM data covering the model domain. Unless
+%             varlist is specified (see below), all 4D fields will be
+%             interpolation onto the open boundaries (1-3D data will be
+%             ignored).
 %   varlist = [optional] cell array of variable (field) names to use from
 %             hycom.
 %
@@ -30,7 +30,7 @@ function Mobj = get_HYCOM_tsobc(Mobj, hycom)
 %
 % EXAMPLE USAGE
 %    hycom = get_HYCOM_forcing(Mobj, [51500, 51531]);
-%    Mobj = get_HYCOM_tsobc(Mobj, hycom)
+%    Mobj = get_HYCOM_tsobc(Mobj, hycom, {'u', 'v', 'temperature', 'salinity'})
 %
 % Author(s):
 %    Pierre Cazenave (Plymouth Marine Laboratory)
@@ -58,10 +58,30 @@ if license('test', 'Distrib_Computing_Toolbox')
     end
 end
 
-[~, ~, nz, nt] = size(hycom.temperature.data);
+if nargin == 2
+    fields = fieldnames(hycom);
+else
+    % We always want Depth because we need it to interpolate the vertical
+    % component.
+    fields = unique(['Depth', varlist]);
+end
+
+assert(isfield(hycom, 'Depth'), 'Require a depth vector to interpolate vertically.')
+
+% Find the first 4D array and use it to get the number of vertical levels
+% and time steps.
+for ff = 1:length(fields)
+    if isfield(hycom.(fields{ff}), 'data') && ndims(hycom.(fields{ff}).data) > 3
+        [nx, ny, nz, nt] = size(hycom.(fields{ff}).data);
+        break
+    end
+end
 
 % Use the existing rectangular arrays for the nearest point lookup.
 [lon, lat] = deal(hycom.lon, hycom.lat);
+
+% Specify the approximate HYCOM resolution.
+hdx = median(diff(hycom.lon(:)));
 
 %oNodes = Mobj.obc_nodes(Mobj.obc_nodes ~= 0);
 % Change the way the nodes are listed to match the order in the
@@ -77,167 +97,222 @@ nf = sum(Mobj.nObcNodes);
 % Number of sigma layers.
 fz = size(Mobj.siglayz, 2);
 
-fvtemp = nan(nf, fz, nt); % FVCOM interpolated temperatures
-fvsal = nan(nf, fz, nt); % FVCOM interpolated salinities
-
 if ftbverbose
     tic
 end
-for t = 1:nt
-    if ftbverbose
-        fprintf('%s : %i of %i timesteps... ', subname, t, nt)
+for v = 1:length(fields)
+
+    if ~(isfield(hycom.(fields{v}), 'data') && ndims(hycom.(fields{v}).data) > 3)
+        continue
     end
-    % Get the current 3D array of PML POLCOMS-ERSEM results.
-    pctemp3 = hycom.temperature.data(:, :, :, t);
-    pcsalt3 = hycom.salinity.data(:, :, :, t);
 
-    % Preallocate the intermediate results arrays.
-    itempz = nan(nf, nz);
-    isalz = nan(nf, nz);
-    idepthz = nan(nf, nz);
+    fvtemp = nan(nf, fz, nt); % FVCOM interpolated data
 
-    for j = 1:nz
-        % Now extract the relevant layer from the 3D subsets. Transpose the
-        % data to be (x, y) rather than (y, x).
-        pctemp2 = pctemp3(:, :, j)';
-        pcsalt2 = pcsalt3(:, :, j)';
-        pcdepth2 = squeeze(hycom.Depth.data(:, :, j, t))';
+    for t = 1:nt
 
-        % Create new arrays which will be flattened when masking (below).
-        tpctemp2 = pctemp2;
-        tpcsalt2 = pcsalt2;
-        tpcdepth2 = pcdepth2;
-        tlon = lon;
-        tlat = lat;
+        % Make a 3D array of the HYCOM depths and mask where we don't have
+        % data. This can then be used in the interpolation instead of trying to
+        % deal with this on the fly.
+        hdepth = permute(repmat(hycom.Depth.data, [1, nx, ny]), [2, 3, 1]);
+        hmask = hycom.(fields{v}).data(:, :, :, t) > 1.26e29;
+        hdepth(hmask) = nan;
 
-        % Create and apply a mask to remove values outside the domain. This
-        % inevitably flattens the arrays, but it shouldn't be a problem
-        % since we'll be searching for the closest values in such a manner
-        % as is appropriate for an unstructured grid (i.e. we're assuming
-        % the HYCOM data is irregularly sampled and interpolation with a
-        % triangulation).
-        mask = tpctemp2 > 1.26e29;
-        tpctemp2(mask) = [];
-        tpcsalt2(mask) = [];
-        tpcdepth2(mask) = [];
-        % Also apply the masks to the position arrays so we can't even find
-        % positions outside the domain, effectively meaning if a value is
-        % outside the domain, the nearest value to the boundary node will
-        % be used.
-        tlon(mask) = [];
-        tlat(mask) = [];
+        if ftbverbose
+            fprintf('%s : %i of %i timesteps... ', subname, t, nt)
+        end
+        % Get the current 3D array of HYCOM results.
+        pctemp3 = hycom.(fields{v}).data(:, :, :, t);
 
-        % Preallocate the intermediate results arrays.
-        itempobc = nan(nf, 1);
-        isalobc = nan(nf, 1);
-        idepthobc = nan(nf, 1);
+        % Preallocate the intermediate results array.
+        itempz = nan(nf, nz);
 
-        % Speed up the tightest loop with a parallelized loop.
-        parfor i = 1:nf
-            % Now we can do each position within the 2D layer.
+        for j = 1:nz
+            % Now extract the relevant layer from the 3D subsets.
+            pctemp2 = pctemp3(:, :, j);
 
-            fx = fvlon(i);
-            fy = fvlat(i);
+            % Create new arrays which will be flattened when masking (below).
+            tpctemp2 = pctemp2;
+            tlon = lon;
+            tlat = lat;
 
-            [~, ii] = sort(sqrt((tlon - fx).^2 + (tlat - fy).^2));
-            % Get the n nearest nodes from PML POLCOMS-ERSEM data (more?
-            % fewer?).
-            ixy = ii(1:16);
+            % Create and apply a mask to remove values outside the domain. This
+            % inevitably flattens the arrays, but it shouldn't be a problem
+            % since we'll be searching for the closest values in such a manner
+            % as is appropriate for an unstructured grid (i.e. we're assuming
+            % the HYCOM data is irregularly sampled and interpolation with a
+            % triangulation).
+            mask = tpctemp2 > 1.26e29;
+            tpctemp2(mask) = [];
 
-            % Get the variables into static variables for the
-            % parallelisation.
-            plon = tlon(ixy);
-            plat = tlat(ixy);
-            ptemp = tpctemp2(ixy);
-            psal = tpcsalt2(ixy);
-            pdepth = tpcdepth2(ixy);
+            % Also apply the masks to the position arrays so we can't even find
+            % positions outside the domain, effectively meaning if a value is
+            % outside the domain, the nearest value to the boundary node will
+            % be used.
+            tlon(mask) = [];
+            tlat(mask) = [];
 
-            % Use a triangulation to do the horizontal interpolation.
-            tritemp = TriScatteredInterp(plon', plat', ptemp', 'natural');
-            trisal = TriScatteredInterp(plon', plat', psal', 'natural');
-            triz = TriScatteredInterp(plon', plat', pdepth', 'natural');
-            itempobc(i) = tritemp(fx, fy);
-            isalobc(i) = trisal(fx, fy);
-            idepthobc(i) = triz(fx, fy);
+            % If the HYCOM depths are now below the maximum depth in the
+            % model domain, we'll have no valid data, so skip the
+            % interpolation and just leave the NaNs in the itempz array.
+            if isempty(tlon) && isempty(tlat)
+                continue
+            end
 
-            % Check all three, though if one is NaN, they all will be.
-            if isnan(itempobc(i)) || isnan(isalobc(i)) || isnan(idepthobc(i))
-                warning('FVCOM boundary node at %f, %f is outside the PML POLCOMS-ERSEM domain. Setting to the closest PML POLCOMS-ERSEM value.', fx, fy)
-                itempobc(i) = tpctemp2(ii(1));
-                isalobc(i) = tpcsalt2(ii(1));
-                idepthobc(i) = tpcdepth2(ii(1));
+            % Preallocate the intermediate results array.
+            itempobc = nan(nf, 1);
+
+            % Speed up the tightest loop with a parallelized loop.
+            parfor i = 1:nf
+                fx = fvlon(i);
+                fy = fvlat(i);
+
+                [dist, ii] = sort(sqrt((tlon - fx).^2 + (tlat - fy).^2));
+                % Get the n nearest nodes from HYCOM data (more? fewer?).
+                ixy = ii(1:16);
+
+                % If the minimum distance away is greater than twice the
+                % HYCOM grid resolution, skip this point at this vertical
+                % level.
+                if min(dist) > 2 * hdx
+                    continue
+                end
+
+                % Get the variables into static variables for the
+                % parallelisation.
+                plon = tlon(ixy);
+                plat = tlat(ixy);
+                ptemp = tpctemp2(ixy);
+
+                % Use a triangulation to do the horizontal interpolation.
+                tritemp = TriScatteredInterp(plon', plat', ptemp', 'natural');
+                itempobc(i) = tritemp(fx, fy);
+
+                if isnan(itempobc(i))
+                    warning('FVCOM boundary node at %f, %f is outside the HYCOM domain. Setting to the closest HYCOM value.', fx, fy)
+                    itempobc(i) = tpctemp2(ii(1));
+
+                    % This might happen if the open boundary falls on
+                    % HYCOM's land. Uncomment the code below to get a plot
+                    % as it's going (you'll need to fall back to a normal
+                    % for loop as opposed to a parfor loop).
+                    %figure
+                    %clf
+                    %plot(tlon(ii(1:500)), tlat(ii(1:500)), '.')
+                    %hold on
+                    %plot(fx, fy, 'rx')
+                    %axis('equal', 'tight')
+                    %pause
+                end
+            end
+
+            % Put the results in the intermediate array.
+            itempz(:, j) = itempobc;
+
+        end
+
+        % If you want to check the interpolation has worked properly:
+        % xx = repmat(fvlon, [1, nz]);
+        % yy = repmat(fvlat, [1, nz]);
+        % zz = repmat(-hycom.Depth.data, [1, nf])';
+        % dd = nanmax(hdepth, [], 3);
+        % cc = itempz;
+        % mm = ~isnan(cc);
+        % ffx = repmat(fvlon, [1, fz]);
+        % ffy = repmat(fvlat, [1, fz]);
+        % ff = Mobj.siglayz(oNodes, :);
+        % figure(10)
+        % clf
+        % scatter3(xx(mm), yy(mm), zz(mm), 40, cc(mm), 'filled')
+        % hold on
+        % scatter3(hycom.lon(:), hycom.lat(:), -dd(:), 40, 'c.')
+        % scatter3(ffx(:), ffy(:), ff(:), 'k.')
+        % colorbar
+        % zlim([-300, 0])
+
+        % Now we've interpolated in space, we can interpolate the z-values
+        % to the sigma depths.
+
+        % Preallocate the output arrays
+        fvtempz = nan(nf, fz);
+
+        for pp = 1:nf
+            % Get the FVCOM depths at this node
+            tfz = Mobj.siglayz(oNodes(pp), :);
+
+            % The HYCOM data is unusual in that the depths are fixed and
+            % data are only saved at the depths shallower than the
+            % bathymetry. As such, we get NaN values below the water depth
+            % and we need to filter those out here.
+
+            % Find the HYCOM depths which cover the modelled depth range.
+            tpz = -hycom.Depth.data(-hycom.Depth.data > min(tfz));
+
+            % To ensure we get the full vertical expression of the vertical
+            % profiles, we need to normalise the POLCOMS-ERSEM and FVCOM
+            % depths to the same range. This is because in instances where
+            % FVCOM depths are shallower (e.g. in coastal regions), if we
+            % don't normalise the depths, we end up truncating the vertical
+            % profile. This approach ensures we always use the full
+            % vertical profile, but we're potentially squeezing it into a
+            % smaller depth.
+            A = max(-hycom.Depth.data);
+            B = min(-hycom.Depth.data);
+            C = max(tfz);
+            D = min(tfz);
+            norm_tpz = (((D - C) * (tpz - A)) / (B - A)) + C;
+
+            % Get the temperature and salinity values for this node and
+            % interpolate down the water column (from PML POLCOMS-ERSEM to
+            % FVCOM). I had originally planned to use csaps for the
+            % vertical interplation/subsampling at each location. However,
+            % the demo of csaps in the MATLAB documentation makes the
+            % interpolation look horrible (shaving off extremes). interp1
+            % provides a range of interpolation schemes, of which pchip
+            % seems to do a decent job of the interpolation (at least
+            % qualitatively).
+            if ~isnan(tpz)
+                fvtempz(pp, :) = interp1(norm_tpz, itempz(pp, :), tfz, 'pchip', 'extrap');
+            else
+                warning('Should never see this... ') % because we test for NaNs when fetching the values.
+                warning('FVCOM boundary node at %f, %f is outside the PML POLCOMS-ERSEM domain. Skipping.', fvlon(pp), fvlat(pp))
+                continue
             end
         end
 
-        % Put the results in the intermediate array.
-        itempz(:, j) = itempobc;
-        isalz(:, j) = isalobc;
-        idepthz(:, j) = idepthobc;
+        % The horizontally- and vertically-interpolated values in the final
+        % FVCOM results array.
+        fvtemp(:, :, t) = fvtempz;
 
-    end
-
-    % Now we've interpolated in space, we can interpolate the z-values
-    % to the sigma depths.
-
-    % Preallocate the output arrays
-    fvtempz = nan(nf, fz);
-    fvsalz = nan(nf, fz);
-
-    for pp = 1:nf
-        % Get the FVCOM depths at this node
-        tfz = Mobj.siglayz(oNodes(pp), :);
-        % Now get the interpolated PML POLCOMS-ERSEM depth at this node
-        tpz = idepthz(pp, :);
-
-        % To ensure we get the full vertical expression of the vertical
-        % profiles, we need to normalise the POLCOMS-ERSEM and FVCOM
-        % depths to the same range. This is because in instances where
-        % FVCOM depths are shallower (e.g. in coastal regions), if we
-        % don't normalise the depths, we end up truncating the vertical
-        % profile. This approach ensures we always use the full
-        % vertical profile, but we're potentially squeezing it into a
-        % smaller depth.
-        A = max(tpz);
-        B = min(tpz);
-        C = max(tfz);
-        D = min(tfz);
-        norm_tpz = (((D - C) * (tpz - A)) / (B - A)) + C;
-
-        % Get the temperature and salinity values for this node and
-        % interpolate down the water column (from PML POLCOMS-ERSEM to
-        % FVCOM). I had originally planned to use csaps for the
-        % vertical interplation/subsampling at each location. However,
-        % the demo of csaps in the MATLAB documentation makes the
-        % interpolation look horrible (shaving off extremes). interp1
-        % provides a range of interpolation schemes, of which pchip
-        % seems to do a decent job of the interpolation (at least
-        % qualitatively).
-        if ~isnan(tpz)
-            fvtempz(pp, :) = interp1(norm_tpz, itempz(pp, :), tfz, 'pchip', 'extrap');
-            fvsalz(pp, :) = interp1(norm_tpz, isalz(pp, :), tfz, 'pchip', 'extrap');
-        else
-            warning('Should never see this... ') % because we test for NaNs when fetching the values.
-            warning('FVCOM boundary node at %f, %f is outside the PML POLCOMS-ERSEM domain. Skipping.', fvlon(pp), fvlat(pp))
-            continue
+        if ftbverbose
+            fprintf('done.\n')
         end
     end
-
-    % The horizontally- and vertically-interpolated values in the final
-    % FVCOM results array.
-    fvtemp(:, :, t) = fvtempz;
-    fvsal(:, :, t) = fvsalz;
-
-    if ftbverbose
-        fprintf('done.\n')
-    end
+    % Dump the data into a temporary structure.
+    fvcom.(fields{v}) = fvtemp;
 end
 if ftbverbose
     toc
 end
 
-Mobj.temperature = fvtemp;
-Mobj.salt = fvsal;
-Mobj.ts_times = hycom.time;
+fvfields = fieldnames(fvcom);
+for s = 1:length(fvcom)
+    switch fvfields{s}
+        case 'temperature'
+            Mobj.temperature = fvcom.temperature;
+        case 'salinity'
+            Mobj.salt = fvcom.salinity;
+        case 'u'
+            Mobj.u = fvcom.u;
+        case 'v'
+            Mobj.v = fvcom.v;
+        otherwise
+            warning('Unrecognised variable %s', fvfields{s})
+    end
+end
+
+if isfield(hycom, 'time')
+    Mobj.ts_times = hycom.time;
+end
 
 % Close the MATLAB pool if we opened it.
 if wasOpened
