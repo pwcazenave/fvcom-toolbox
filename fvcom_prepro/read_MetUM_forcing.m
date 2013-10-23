2063525187f80b29fa591ed33447b48c1bc4385f
Pierre Cazenave <pwcazenave@gmail.com>
2013-10-23 12:14:25 +0100

    Fix the way time is handled when reading in the newest Met Office model outputs (2012 onwards).
    Previously a time variable had to be specified in varlist. Now, each
    data variable's time is returned as an array within the MetUM.(variable)
    struct, giving MetUM.(variable).time and MetUM.(variable).data. This
    means if each data variable uses a different time sampling, that can be
    accounted for later (by interpolating to a common time series with
    interp3, for example).
    
    Currently the code extracts the first 6 hour's worth of data. The
    assumption there is that the Met Office do 4 runs per day, so 6 hours of
    data from each run gives you a day's worth.

--- fvcom_prepro/read_MetUM_forcing.m
+++ fvcom_prepro/read_MetUM_forcing.m
@@ -45,6 +45,15 @@ function MetUM = read_MetUM_forcing(files, varlist)
 %   was previously using four times daily forcing, I'm not that fussed.
 %   2013-09-12 Add support for extracting the surface pressure level from
 %   the 4D temperature variable (temp_2).
+%   2013-10-23 Fix the way time is handled. Previously a time variable had
+%   to be specified in varlist. Now, each data variable's time is returned
+%   as an array within the MetUM.(variable) struct, giving
+%   MetUM.(variable).time and MetUM.(variable).data. This means if each
+%   data variable uses a different time sampling, that can be accounted for
+%   later (by interpolating to a common time series with interp3, for
+%   example). Currently the code extracts the first 6 hour's worth of data.
+%   The assumption there is that the Met Office do 4 runs per day, so 6
+%   hours of data from each run gives you a day's worth.
 %
 %==========================================================================
 
@@ -79,17 +88,15 @@ if isempty(levelidx)
     levelidx = 5;
 end
 
-% Set the number of time steps to extract (6 seems to exclude the 4
-% forecast time steps in the pre-2012 data and also matches the total
-% number of outputs in the 2012 and onwards data).
-% TODO: Add check against number of loaded times in tmpdata and set nh to
-% minimum of nh or number of loaded times
-nh = 6;
-
 MetUM = struct();
 
 for f = 1:length(files)
-    
+
+    % Set the number of time steps to extract to default to 6. It should be
+    % checked for each file anyway (assuming there's a time variable being
+    % requested).
+    nh = 6;
+
     if ftbverbose
         % Don't display the full path if it's really long.
         if length(files{f}) > 80
@@ -102,7 +109,7 @@ for f = 1:length(files)
     end
 
     nc = netcdf.open(files{f}, 'NOWRITE');
-    
+
     % Query the netCDF file to file the variable names. If the name matches
     % one in the list we've been given (or if we haven't been given any
     % particular variables), save it in the output struct.
@@ -130,20 +137,41 @@ for f = 1:length(files)
                 switch varname
                     case {'x', 'y', 'x_1', 'y_1', 'longitude', 'latitude'}
                         continue
-                    case {'t', 't_1'}
-                        % Get the time attribute so we can store proper
-                        % times.
-                        tt = fix_time(nc, varid, varAtts, nh);
-                        MetUM.(safename) = cat(1, MetUM.(safename), tt);
+                    case {'t', 't_1', 't_2', 't_3', 't_4', 't_5', 't_6', 't_7', 't_8'}
+                        % Ignore time variables.
+                        continue
                     otherwise
                         try
+                            % Extract the time for this variable.
+                            temptime = fix_time(nc, varid);
+
+                            % Find how many indices to extract to at least
+                            % 6 hours of data.
+                            interval = mean(roundn(diff(datenum(temptime)) * 24 * 60, 0));
+                            if abs(60 - interval) < abs(30 - interval)
+                                % Hourly
+                                nh = 6;
+                            elseif abs(30 - interval) < abs(60 - interval)
+                                % Half-hourly
+                                nh = 12;
+                            else
+                                error('Unsupported time sampling interval (support hourly and half-hourly sampling).')
+                            end
+                            % Check we don't try and get more data than we
+                            % have.
+                            if nh > size(temptime, 1);
+                                nh = size(temptime, 1);
+                            end
+
+                            MetUM.(safename).time = [MetUM.(safename).time; temptime(1:nh, :)];
+
                             % Append along last dimension.
                             if nn == 3
-                                MetUM.(safename) = cat(nn, MetUM.(safename), tmpdata(:, :, 1:nh));
+                                MetUM.(safename).data = cat(nn, MetUM.(safename).data, tmpdata(:, :, 1:nh));
                             else
                                 % We're flattening from 4D to 3D here, so
                                 % nn - 1.
-                                MetUM.(safename) = cat(nn - 1, MetUM.(safename), squeeze(tmpdata(:, :, levelidx, 1:nh)));
+                                MetUM.(safename).data = cat(nn - 1, MetUM.(safename).data, squeeze(tmpdata(:, :, levelidx, 1:nh)));
                             end
                         catch err
                             fprintf('\n')
@@ -155,19 +183,43 @@ for f = 1:length(files)
             else % first time around
                 switch varname
                     case {'x', 'y', 'x_1', 'y_1', 'longitude', 'latitude'}
-                        MetUM.(safename) = tmpdata;
-                    case {'t', 't_1'}
-                        % Get the time attribute so we can store proper
-                        % times.
-                        MetUM.(safename) = fix_time(nc, varid, varAtts, nh);
+                        MetUM.(safename).data = tmpdata;
+                    case {'t', 't_1', 't_2', 't_3', 't_4', 't_5', 't_6', 't_7', 't_8'}
+                        % Ignore time variables.
+                        continue
                     otherwise
+                        % This is data.
+
+                        % Extract the time for this variable.
+                        temptime = fix_time(nc, varid);
+
+                        % Find how many indices to extract to at least
+                        % 6 hours of data.
+                        interval = mean(roundn(diff(datenum(temptime)) * 24 * 60, 0));
+                        if abs(60 - interval) < abs(30 - interval)
+                            % Hourly
+                            nh = 6;
+                        elseif abs(30 - interval) < abs(60 - interval)
+                            % Half-hourly
+                            nh = 12;
+                        else
+                            error('Unsupported time sampling interval (support hourly and half-hourly sampling).')
+                        end
+                        % Check we don't try and get more data than we
+                        % have.
+                        if nh > size(temptime, 1);
+                            nh = size(temptime, 1);
+                        end
+
+                        MetUM.(safename).time = temptime(1:nh, :);
+
                         if nn == 3
-                            MetUM.(safename) = tmpdata(:, :, 1:nh);
+                            MetUM.(safename).data = tmpdata(:, :, 1:nh);
                         else
                             % Assume temperature at pressure levels.
                             % Extract the 1000mb pressure level
                             % (approximately the surface).
-                            MetUM.(safename) = squeeze(tmpdata(:, :, levelidx, 1:nh));
+                            MetUM.(safename).data = squeeze(tmpdata(:, :, levelidx, 1:nh));
                         end
                 end
             end
@@ -190,32 +242,43 @@ if ftbverbose
     fprintf('end   : %s \n', subname)
 end
 
-function tt = fix_time(nc, varid, varAtts, nh)
-% Little helper function to get the time attribute so we can store proper
-% times.
+function fixedtime = fix_time(nc, varid)
+% Little helper function to get the time data for the current variable.
 %
 % INPUT:
 %   nc : netCDF file handle
 %   varid : current variable ID
-%   varAtts : number of variable attributes
-%   nh : number of time steps to extract
 %
 % OUTPUT:
 %   tt : date string for the current file (Gregorian date)
-%
-for j = 1:varAtts
-    timeatt = netcdf.inqAttName(nc, varid, j - 1);
+
+% Extract the time array for this variable's time dimension.
+[numdims, ~, ~, ~] = netcdf.inq(nc);
+dimnames = cell(numdims, 1);
+for jj = 1:numdims
+    [dimname, ~] = netcdf.inqDim(nc, jj - 1);
+    dimnames{jj} = dimname;
+end
+
+% Find the dimensions of this variable.
+[~, ~, dimids, ~] = netcdf.inqVar(nc, varid);
+% We presume the time variable starts with a t.
+ttidx = strncmpi(dimnames(dimids + 1), 't', length('t'));
+ttvarid = netcdf.inqVarID(nc, dimnames{dimids(ttidx) + 1});
+% There are issues around precision here, so
+% convert tt to minutes and then back to fractions
+% of a day.
+tt = netcdf.getVar(nc, ttvarid, 'double');
+tt = roundn(tt * 24 * 60, -1) / 24 / 60;
+
+[~, ~, ~, tVarAtts] = netcdf.inqVar(nc, ttvarid);
+
+for j = 1:tVarAtts
+    timeatt = netcdf.inqAttName(nc, ttvarid, j - 1);
     if strcmpi(timeatt, 'time_origin')
-        timeval = netcdf.getAtt(nc, varid, timeatt);
+        timeval = netcdf.getAtt(nc, ttvarid, timeatt);
     end
 end
 mt = datenum(timeval, 'dd-mmm-yyyy:HH:MM:SS');
-% t is the offset in days relative to mt.
-t = netcdf.getVar(nc, varid, 'double');
-if isempty(t)
-    % This isn't right. Might be easier to get the time from the file
-    % name...
-    t = 0;
-end
-% Add the offset and convert back to a date string.
-tt = datestr(mt + t(1:nh), 'yyyy-mm-dd HH:MM:SS');
+
+fixedtime = datestr(mt + tt, 'yyyy-mm-dd HH:MM:SS');
