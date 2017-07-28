a0cc0ec648d4fdbf1e4eb8e5e4d9413faf9f0cad
Pierre Cazenave <pica@pml.ac.uk>
2017-07-28 15:06:15 +0100

    Minor clean up of the code to read in the grid bathymetry.

--- fvcom_prepro/read_fvcom_bath.m
+++ fvcom_prepro/read_fvcom_bath.m
@@ -1,14 +1,14 @@
-function [h] = read_fvcom_bath(bathfile) 
+function [h] = read_fvcom_bath(bathfile)
 
-% Read fvcom bathymetry file 
+% Read fvcom bathymetry file
 %
 % [h] = function read_fvcom_bath(bathfile)
 %
 % DESCRIPTION:
-%    Read FVCOM Bathymetry file 
+%    Read FVCOM Bathymetry file
 %
-% INPUT [keyword pairs]:  
-%   'bathfile'  = fvcom bathymetry file
+% INPUT:
+%   bathfile  = fvcom bathymetry file
 %
 % OUTPUT:
 %    h = bathymetry vector
@@ -16,13 +16,15 @@ function [h] = read_fvcom_bath(bathfile)
 % EXAMPLE USAGE
 %    Mobj = read_fvcom_bath('tst_dep.dat')
 %
-% Author(s):  
+% Author(s):
 %    Geoff Cowles (University of Massachusetts Dartmouth)
 %    Rory O'Hara Murray (Marine Scotland Science)
+%    Pierre Cazenave (Plymouth Marine Laboratory)
 %
 % Revision history
 %    2014-11-19 Remove loops to speed up reading in the file.
-%   
+%    2017-04-11 Minor clean up of the code.
+%
 %==============================================================================
 
 global ftbverbose
@@ -34,25 +36,24 @@ end
 %------------------------------------------------------------------------------
 % read in the FVCOM bathymetry data
 %------------------------------------------------------------------------------
-fid = fopen(bathfile,'r');
-if(fid  < 0)
-  error(['file: ' bathfile ' does not exist']);
-end;
+fid = fopen(bathfile, 'r');
+assert(fid > 0, 'file: %s does not exist', bathfile);
+
 C = textscan(fid, '%s %s %s %d', 1);
 Nverts = C{4};
-h = zeros(Nverts,1);
-fprintf('reading bathymetry file\n');
-fprintf('# nodes %d\n',Nverts);
+if ftbverbose
+    fprintf('reading bathymetry file\n');
+    fprintf('# nodes %d\n',Nverts);
+end
 C = textscan(fid,' %f %f %f',Nverts);
 h = C{3};
-fprintf('min depth %f max depth %f\n',min(h),max(h));
-fprintf('bathymetry reading complete\n');
 fclose(fid);
 
+if ftbverbose
+    fprintf('min depth %f max depth %f\n',min(h),max(h));
+    fprintf('bathymetry reading complete\n');
+end
 
-
-if(ftbverbose)
-  fprintf(['end   : ' subname '\n'])
-end;
-
-
+if ftbverbose
+    fprintf('end   : %s\n', subname)
+end
